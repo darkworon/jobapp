7749ce60d8546519f879b51ed53c83ba83dc4751
Oleg Mickhaylov <woron@nights.su>
2012-04-22 19:29:43 +0400

    fixed a typo in capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,8 @@ after 'deploy:update_code', :roles => :app do
   run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
   run "cd #{current_release} && bundle exec whenever -w && bundle exec whenever -u"
   run "cd #{current_release} && bundle exec rake db:migrate RAILS_ENV=production"
-  run "cd #{current_release} && bundle exec rake ts:restart RAILS_ENV=production"
+  run "cd #{deploy_to}/current && bundle exec rake ts:stop RAILS_ENV=production"
+  run "cd #{current_release} && bundle exec rake ts:start RAILS_ENV=production"
   run "cd #{current_release} && bundle exec rake -s sitemap:refresh RAILS_ENV=production"
   run "RAILS_ENV=production #{current_release}/script/delayed_job restart"
 end
